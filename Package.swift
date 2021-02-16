9d91ff6863de052775c43849741e019f15d8a96b
Adrian Bilescu <adrian.bilescu@gmail.com>
2021-02-16 10:33:11 +0200

    Fixed sources & path

--- Package.swift
+++ Package.swift
@@ -21,9 +21,14 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        
         .target(
             name: "SwiftyDrop",
-            dependencies: []),
+            path: "Sources/SwiftyDrop",
+            exclude: [
+                "Info.plist", "SwiftyDrop.h"
+            ]
+        ),
         .testTarget(
             name: "SwiftyDropTests",
             dependencies: ["SwiftyDrop"]),
